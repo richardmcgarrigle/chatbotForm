d91bcd017ddc2fc07fe4616546fe45bf178dd2e5
richard & mark <sdfds@sdfds.com>
2017-07-12 11:59:30 +0100

    add name to initial prompt

--- messages/index.js
+++ messages/index.js
@@ -22,7 +22,7 @@ bot.localePath(path.join(__dirname, './locale'));
 
 bot.dialog('/', [
     function (session) {
-        builder.Prompts.choice(session, "Hello... I am a claims bot, you can use me to send photos of evidence for your claim new claim. To get started I will need to know if you will be asking about a new or existing claim.", ['new', 'existing']);
+        builder.Prompts.choice(session, "Hello" + session.name + " - I am a claims bot, you can use me to send photos of evidence for your claim new claim. To get started I will need to know if you will be asking about a new or existing claim.", ['new', 'existing']);
     },
     function (session, results) {
         session.userData.type = results.response;
