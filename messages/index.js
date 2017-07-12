1026ab4c32e9e84a80211883999059a9fb70f2c8
richard & mark <sdfds@sdfds.com>
2017-07-12 11:18:55 +0100

    add number intent

--- messages/index.js
+++ messages/index.js
@@ -25,8 +25,14 @@ bot.dialog('/', [
         builder.Prompts.choice(session, "Hello... I am a claims bot, you can use me to send photos of evidence for your claim new claim. To get started I will need to know if you will be asking about a new or existing claim.", ['new', 'existing']);
     },
     function (session, results) {
-        session.userData.policy = results.response;
-        builder.Prompts.attachment(session, "Got it " + results.response + ", looking that up now. While I'm doing that please send me the photo you would like attached to your policy for this cliam.");
+        session.userData.type = results.response;
+        if(session.userData.type 'new'){
+            builder.Prompts.number(session, "Ok, a " + session.userData.type + " claim, I'll need your policy number to start, please enter that below");
+        }
+        else if(session.userData.type 'existing'){
+            builder.Prompts.number(session, "Ok, an " + session.userData.type + " claim, I'll need your claim number to start, please enter that below");
+        }
+
     },
     function (session, results) {
         session.userData.policy = results.response;
